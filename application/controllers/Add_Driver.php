eedd292ea3f449fc000f474f3cce1d05f5896f38
hamd mahmood <116299051+mahmoodhamd@users.noreply.github.com>
2024-01-02 20:58:01 +0500

    adddriver change

--- application/controllers/Add_Driver.php
+++ application/controllers/Add_Driver.php
@@ -108,6 +108,9 @@ function show_driver_id($id) {
 
 $data['single_driver'] = $this->staff->show_driver_id($id);
 $this->load->view('edit_driver', $data);
+// dsajskdjsldjasd
+// this is the future life yooo man
+// i don't regret thoo.......
 }
 
 function update_driver_id() {
